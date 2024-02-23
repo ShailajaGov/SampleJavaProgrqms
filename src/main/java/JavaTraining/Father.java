a2606b961f99a888f9910a096e363451994fa75e
ShailajaGov <shailaja.sireesh@gmail.com>
2024-02-23 15:57:24 -0800

    team member changes

--- src/main/java/JavaTraining/Father.java
+++ src/main/java/JavaTraining/Father.java
@@ -2,11 +2,11 @@ package JavaTraining;
 
 public class Father {
 
-	
+	//team member changes 1
 		public void bike()
 		{
 			System.out.println("Fathers bike - Hero honda");
 		}
 	
-
+//team member changes 2
 }
--- src/main/java/JavaTraining/Son.java
+++ src/main/java/JavaTraining/Son.java
@@ -8,6 +8,7 @@ public class Son extends Father{
 		System.out.println("Son's bike - Pulsar");
 	}
 	
+	//team member changes
 	public void car()
 	{
 		System.out.println("Son's car Swift");
