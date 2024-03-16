86812392f4ff4e0dc91977ac9692bc1da2bc888b
ShailajaGov <shailaja.sireesh@gmail.com>
2024-03-15 20:56:49 -0700

    updated 2 files

--- src/main/java/JavaTraining/GitDiffTestingClass.java
+++ src/main/java/JavaTraining/GitDiffTestingClass.java
@@ -5,7 +5,15 @@ public class GitDiffTestingClass {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//updated by team member
+		//line 1 team member
 
+	}
+	
+	public void testMethod()
+	{
+		//team member
 	}
 
 }
